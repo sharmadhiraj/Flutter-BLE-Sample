902d25c6ddf0e021d19cfaebcc8414239c8373ae
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2024-04-04 16:33:28 +0545

    Implement scan devices functionality.

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 A simple Flutter app demonstrating Bluetooth Low Energy (BLE) functionality.
 
-## Work In Progress
+### Features
+
+- [x] Scan devices
+- [ ] Connect device
+- [ ] Read characteristics
+- [ ] Write characteristics
+
+Your feedback and ideas are valuable to me as I continue to improve this repository. Feel free to
+reach out with any questions or suggestions. Happy coding!
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ble_sample/util/constant.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<BluetoothDevice> devices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    startScanning();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +31,39 @@ class HomeScreen extends StatelessWidget {
   }
 
   Widget _buildBody() {
-    return Container();
+    return ListView.builder(
+      itemCount: devices.length,
+      itemBuilder: (context, index) => _buildListItem(devices[index]),
+    );
+  }
+
+  Widget _buildListItem(BluetoothDevice device) {
+    return Card(
+      child: ListTile(
+        title: Text(device.remoteId.str),
+      ),
+    );
+  }
+
+  void startScanning() async {
+    await FlutterBluePlus.adapterState
+        .where((val) => val == BluetoothAdapterState.on)
+        .first;
+    FlutterBluePlus.scanResults.listen(
+      (results) {
+        for (ScanResult result in results) {
+          if (!devices.contains(result.device)) {
+            setState(() => devices.add(result.device));
+          }
+        }
+      },
+    );
+    await FlutterBluePlus.startScan();
+  }
+
+  @override
+  void dispose() {
+    FlutterBluePlus.stopScan();
+    super.dispose();
   }
 }
